3baa42d4f21d079fb3a939481ed4df62da041911
Dioo16 <Borgesdiogo54@gmail.com>
2021-11-11 12:50:17 -0300

    Resolvendo conflitos 2

--- src/app/components/home/home.module.ts
+++ src/app/components/home/home.module.ts
@@ -13,7 +13,7 @@ const routes: Routes = [
 
 @NgModule({
   declarations: [
-    HomeComponent
+  HomeComponent
   ],
   imports: [
     CommonModule,
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,14 @@
 export const environment = {
   production: false,
+   firebaseConfig:{
+    apiKey: "AIzaSyDBUcXgWHr9-uAle8GQwgdc8JTWEY5wC5w",
+    authDomain: "treinandofirebase-e5fb1.firebaseapp.com",
+    projectId: "treinandofirebase-e5fb1",
+    storageBucket: "treinandofirebase-e5fb1.appspot.com",
+    messagingSenderId: "43768733951",
+    appId: "1:43768733951:web:e2ce7892f072427bb06cca",
+    measurementId: "G-XPZPFRTXXR",
+  },
 
   baseUrl: 'https://api-content.ingresso.com/v0/',
   title: 'OsascoCines'
