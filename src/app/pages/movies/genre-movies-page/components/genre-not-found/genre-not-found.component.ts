0044b4263f162af497b3b4b805425a6c7a7bbb58
Gustavo Reis <gustavo.reis20000@gmail.com>
2021-11-19 15:24:24 -0300

    genres-movies fixed

--- src/app/pages/movies/genre-movies-page/components/genre-not-found/genre-not-found.component.ts
+++ src/app/pages/movies/genre-movies-page/components/genre-not-found/genre-not-found.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-genre-not-found',
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GenreNotFoundComponent implements OnInit {
 
+  @Output() hasMovie = new EventEmitter<any>();
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  setHasMovie() {
+    this.hasMovie.emit();
+  }
 }
