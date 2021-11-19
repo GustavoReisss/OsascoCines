1a91825789c4029b3e9815f46ef0d56e5796cf49
Gustavo Reis <gustavo.reis20000@gmail.com>
2021-11-19 11:50:39 -0300

    mudanças no perfil do usuário

--- src/app/pages/user/user.module.ts
+++ src/app/pages/user/user.module.ts
@@ -3,16 +3,16 @@ import { CommonModule } from '@angular/common';
 import { Route, RouterModule} from '@angular/router';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { BaseTemplateComponent } from 'src/app/shared/templates/base-template/base-template.component';
 
 const routes: Route[] = [
   {
     path: "",
-    redirectTo: "perfil",
-    pathMatch: "full"
-  },
-  {
-    path: "perfil",
-    component: DashboardComponent
+    component: BaseTemplateComponent,
+    children: [
+      { path: "", redirectTo: "perfil", pathMatch: "full" },
+      { path: "perfil", component: DashboardComponent }
+    ]
   }
 ]
 
