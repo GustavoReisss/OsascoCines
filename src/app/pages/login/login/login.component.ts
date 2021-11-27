3b9a79395ff70af78e563f7aef3298b3cabdca39
Gustavo Reis <gustavo.reis20000@gmail.com>
2021-11-27 19:10:09 -0300

    login consertado

--- src/app/pages/login/login/login.component.ts
+++ src/app/pages/login/login/login.component.ts
@@ -4,7 +4,9 @@ import { AuthService } from 'src/app/shared/services/auth.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: [
+    './login.component.scss'
+  ]
 })
 export class LoginComponent implements OnInit {
 
